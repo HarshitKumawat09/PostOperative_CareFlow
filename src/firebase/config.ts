1ff4e186f72cdd6d08b51c8ff27f740cef84e86b
HarshitKumawat09 <harshitkumawat0910@gmail.com>
2026-03-02 11:45:13 +0530

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,4 @@
-// export const firebaseConfig = {
-//   "projectId": "studio-2363340782-5ffd7",
-//   "appId": "1:567123113004:web:088eb299050fb14d5ecd89",
-//   "apiKey": "GOOGLE_API_KEY",
-//   "authDomain": "studio-2363340782-5ffd7.firebaseapp.com",
-//   "measurementId": "",
-//   "messagingSenderId": "567123113004"
-// };
+
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
